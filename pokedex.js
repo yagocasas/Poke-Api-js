f005aeab47bba6d89de645f132406b7eced69c9a
yagocasas <yagocd10@hotmail.com>
2022-10-18 00:29:20 +0200

    REFACTOR: add typecolors, types, no results

--- pokedex.js
+++ pokedex.js
@@ -1,79 +1,107 @@
-const pokedex$$ = document.querySelector('#pokedex');
+const pokedex$$ = document.querySelector("#pokedex");
 console.log(pokedex$$);
-const ALL_POKEMONS_INFO = [];// Cuando una variable se declara en scope global para ser usada por otros, se hace en mayúsculas.
+const ALL_POKEMONS_INFO = [];
 
-function getAllPokemons () {
-    return fetch ('https://pokeapi.co/api/v2/pokemon/?limit=151')
+const getAllPokemons = () =>
+  fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
     .then((response) => response.json())
-    .then((response) => {
-        return response.results
-    })
-    .catch((error) => console.log('Error obteniendo todos los pokemons', error));
+    .then((response) => response.results)
+    .catch((error) =>
+      console.log("Error obteniendo todos los pokemons", error)
+    );
+
+const getOnePokemon = async (url) => {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+
+    const pokemon = {
+      name: result.name,
+      id: result.id,
+      types: result.types.map((element) => element.type.name),
+      image: result.sprites.other.dream_world.front_default,
+    };
+
+    return pokemon;
+  } catch (error) {
+    console.log("Error obteniendo cada pokemon " + url, error);
+  }
 };
 
-function getOnePokemon (url) {
-    return fetch (url)
-    .then((response) => response.json())
-    .then((response) => {
-        return response
-    })
-    .catch((error) => console.log ('Error obteniendo pokemon individualmente', error))
+const renderTypes = (types, container) => {
+  const div$$ = document.createElement("div");
+  div$$.classList.add("card-subtitle", "types-container");
+
+  types.forEach((type) => {
+    const typeContainer$$ = document.createElement("p");
+    typeContainer$$.setAttribute("pokemon-type", type);
+    typeContainer$$.style.backgroundColor = typeColors[type];
+    typeContainer$$.classList.add("type");
+    typeContainer$$.textContent = type;
+
+    div$$.appendChild(typeContainer$$);
+  });
+
+  container.appendChild(div$$);
 };
 
-/** Aquí hago la prueba directamente en el html y me sirve luego de guía pasándolo a este archivo
- * <li class="card">
-          <img class="card-image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="">
-          <p class="card-title">Bulbasur</p>
-          <div class="card-subtitle">Tipo Fuego</div>
-        </li>} pokemons 
- */
+const cleanPokedex = () => (pokedex$$.innerHTML = "");
 
+const renderNoResults = () => {
+  const li$$ = document.createElement("li");
 
-function renderPokemons(pokemons) {
+  const p$$ = document.createElement("p");
+  p$$.classList.add("card-title");
+  p$$.textContent = "No se encuentran resultados";
 
-    pokemons.forEach((poke) => {
-    
-    const li$$ = document.createElement('li');
-    li$$.classList.add('card');
+  li$$.appendChild(p$$);
+  pokedex$$.appendChild(li$$);
+};
 
-    const img$$ = document.createElement('img');
-    img$$.src = poke.sprites.front_default;
-    img$$.alt = poke.name;
+const renderPokemonCard = (poke) => {
+  const li$$ = document.createElement("li");
+  li$$.classList.add("card");
 
-    const p$$ = document.createElement('p');
-    p$$.classList.add('card-title');
-    p$$.textContent = poke.name;
+  const img$$ = document.createElement("img");
+  img$$.src = poke.image;
+  img$$.alt = poke.name;
+  img$$.classList.add("imagen");
 
-    const div$$ = document.createElement('div');
-    div$$.classList.add('card-subtitle');
-    div$$.textContent = poke.types[0].type.name;
+  // const divInfo$$ = document.createElement("div");
+  // divInfo$$.classList.add("divInfo");
 
-    li$$.appendChild(img$$);
-    li$$.appendChild(p$$);
-    li$$.appendChild(div$$);
+  const h5$$ = document.createElement("h5");
+  h5$$.classList.add("card-title");
+  h5$$.textContent = poke.name;
 
-    pokedex$$.appendChild(li$$);
+  const divId$$ = document.createElement("div");
+  divId$$.classList.add("card-subtitle");
+  divId$$.textContent = "#" + poke.id;
 
-    });
+  li$$.appendChild(img$$);
+  li$$.appendChild(divId$$);
+  li$$.appendChild(h5$$);
 
-}
+  renderTypes(poke.types, li$$);
 
-//esta función es la que me va a generar todo. Creo funciones fuera y las llamo aquí.
-async function init () {
-    console.log('Ejecutando peticiones pokédex');
+  pokedex$$.appendChild(li$$);
+};
 
-    const allPokemons = await getAllPokemons();//hemos guardado en allPokemons el array de objetos con name y url de cada pokemon
-    // console.log('allPokemons:', allPokemons);
+const renderPokemons = (pokemons) => {
+  cleanPokedex();
+  if (!pokemons.length) renderNoResults();
+  pokemons.forEach((pokemon) => renderPokemonCard(pokemon));
+};
 
-    for (const pokemon of allPokemons) {
-        const pokemonInfo = await getOnePokemon(pokemon.url);
-        ALL_POKEMONS_INFO.push(pokemonInfo);
-    }
-    console.log('allPokemonsInfo', ALL_POKEMONS_INFO);
-    renderPokemons(ALL_POKEMONS_INFO);
-}
+const init = async () => {
+  const allPokemons = await getAllPokemons();
 
+  for (const pokemon of allPokemons) {
+    const pokemonInfo = await getOnePokemon(pokemon.url);
+    ALL_POKEMONS_INFO.push(pokemonInfo);
+  }
+  console.log("allPokemonsInfo", ALL_POKEMONS_INFO);
+  renderPokemons(ALL_POKEMONS_INFO);
+};
 
 window.onload = init;
-
-
